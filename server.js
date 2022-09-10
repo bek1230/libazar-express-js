241ce48a916b24f6c5edd26376f39509658b8a0e
Behzod <behzodrasulov432@gmail.com>
2022-09-10 19:24:50 +0500

    kjh

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.get("/", (req, res) => {
     res.send(data);
   });
 });
-
 app.get("/openCard", (req, res) => {
   const postId = req.query.id.slice(0, 36);
   const filePath = path.resolve(__dirname, "./build", "index.html");
@@ -56,127 +55,12 @@ app.get("/openCard", (req, res) => {
       });
   });
 });
-app.get("/login", (req, res) => {
-  const filePath = path.resolve(__dirname, "./build", "index.html");
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-
-    data = data
-      .replace(/__TITLE__/g, "Libazar")
-      .replace(
-        /__DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_OG_IMAGE__/g,
-        "https://firebasestorage.googleapis.com/v0/b/libazar.appspot.com/o/images%2Fquloq.jpeg?alt=media&token=efcf6f59-1a84-4512-b562-d6bcfe329df7"
-      );
-    res.send(data);
-  });
-});
-app.get("/profile", (req, res) => {
-  const filePath = path.resolve(__dirname, "./build", "index.html");
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-
-    data = data
-      .replace(/__TITLE__/g, "Libazar")
-      .replace(
-        /__DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_OG_IMAGE__/g,
-        "https://firebasestorage.googleapis.com/v0/b/libazar.appspot.com/o/images%2Fquloq.jpeg?alt=media&token=efcf6f59-1a84-4512-b562-d6bcfe329df7"
-      );
-    res.send(data);
-  });
-});
-app.get("/about", (req, res) => {
-  const filePath = path.resolve(__dirname, "./build", "index.html");
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-
-    data = data
-      .replace(/__TITLE__/g, "Libazar")
-      .replace(
-        /__DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_OG_IMAGE__/g,
-        "https://firebasestorage.googleapis.com/v0/b/libazar.appspot.com/o/images%2Fquloq.jpeg?alt=media&token=efcf6f59-1a84-4512-b562-d6bcfe329df7"
-      );
-    res.send(data);
-  });
-});
-app.get("/fowarids", (req, res) => {
-  const filePath = path.resolve(__dirname, "./build", "index.html");
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-
-    data = data
-      .replace(/__TITLE__/g, "Libazar")
-      .replace(
-        /__DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_OG_IMAGE__/g,
-        "https://firebasestorage.googleapis.com/v0/b/libazar.appspot.com/o/images%2Fquloq.jpeg?alt=media&token=efcf6f59-1a84-4512-b562-d6bcfe329df7"
-      );
-    res.send(data);
-  });
-});
-app.get("/products", (req, res) => {
-  const filePath = path.resolve(__dirname, "./build", "index.html");
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-
-    data = data
-      .replace(/__TITLE__/g, "Libazar")
-      .replace(
-        /__DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_DESCRIPTION__/g,
-        "Tezkor yetkazib berish xizmati  Buyurtmangiz O'zbekistonning barcha viloyatlariga 3 kun ichida yetqazib beriladi."
-      )
-      .replace(
-        /__META_OG_IMAGE__/g,
-        "https://firebasestorage.googleapis.com/v0/b/libazar.appspot.com/o/images%2Fquloq.jpeg?alt=media&token=efcf6f59-1a84-4512-b562-d6bcfe329df7"
-      );
-    res.send(data);
-  });
-});
 app.use("/assets", express.static("./assets/"));
-
+// app.configure(function() {
+//   app.use('/', express.static(__dirname + '/'));
+// });
+// app.get('*', function(request, response, next) {
+//   response.sendfile(__dirname + './build/index.html');
+// });
 app.use(express.static(path.join(__dirname, "./build")));
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
